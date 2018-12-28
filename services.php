b4a20a3a468eba5ff2665bb23eef6171bcde256d
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-12-27 23:37:04 -0500

    services demo set up

--- services.php
+++ services.php
@@ -8,6 +8,51 @@
                 <h1 class="page-header">
                     SERVICES
                 </h1> 
+                <img src="images/groupExpecting.png" alt="">
+                <br>
+
+                <br>
+                <h2>PREGNANCY AND LABOR SUPPORT</h2>
+                <small>INFORMATIVE, EVIDENCE BASED, AND COMPASSIONATE</small>
+​                <ul>
+                    <li>A free consultation (meet and greet)</li>
+                    <li>Prenatal visit - support methods will be described in detail to help you have the best experience possible and ease fear and anxiety</li>
+                    <li>Assistance with developing birth plan and prenatal information</li>
+                    <li>Unlimited support via phone or email during pregnancy</li>
+                    <li>24/7 on call availability starting at 38 weeks and ending at birth</li>
+                    <li>Continuous support beginning within 2 hours of your call at your home or hospital and ending 1-3 hours postpartum</li>
+                    <li>A timeline of your birth and some photos (if time permits)</li>
+                    <li>A one hour postpartum visit in your home sometime between 3-10 days postpartum and a followup if needed to get you settled in and off to the best start possible.</li>
+                    <li>Available by phone or email for 2-6 weeks past birth</li>
+                    <li>A referral network to help meet your needs outside of my scope of practice</li>
+                    <li>A backup doula available to you in case of emergency</li>
+                </ul>
+                <p> Pricing is listed under specific service packages. Additional services can be added. </p>
+                <h4 style="background:pink; margin: 30px; padding: 10px; border-radius: 10px">Every birth is unique. 
+                    <br>
+                    <br>
+                    Whether it takes place at a hospital, birthing center, or at home, each birth is beautiful and deserving of personalized support.No matter how or where you choose to birth your baby, you can benefit from having a professional birth assistant. I am with you all the way to guide you and support you so  that you feel less stress, less discomfort, and feel that YOU are in charge of your birth. I am there for your support during your most vulnerable time so you can focus on the most important things. 
+                    <br>
+                    <br>
+                    You deserve a doula.
+                </h4>
+
+                <h2>Mother Nourish Session</h2>
+                <small>A Mother Nourish Session is a one-time visit that focuses on filling the mother’s cup.</small>
+                <p> It’s a 2hr session focusing on mental, physical and emotional support during the postpartum period including tea, snacks, bodywork, peer support, discussion and comfort products from New Mama Columbus. Mothering is draining and tireless work. The mother needs to be nourished so she can nourish and nurtured so she can nurture. Infants are biologically programmed to absorb the nurturing the mother is receiving as well. Our infants and our mothers need their village back.
+                    <br>
+                    <br>
+                    $95
+                    <br>
+                    <br>
+                    Includes:
+                    <ul>
+                        <li>tea & snacks</li>
+                        <li>bodywork</li>
+                        <li>peer support & discussion</li>
+                        <li>comfort products from New Mama Columbus</li>
+                    </ul>
+                </p>
             </div>
            <?php include 'includes/sidebar.php'; ?>
         </div>
